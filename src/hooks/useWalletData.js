c3dfee244db4f34bb21e6eb17e3b8098323dcfb8
jameslange <jlange101592@gmail.com>
2022-02-21 09:01:36 +0100

    Updates

--- src/hooks/useWalletData.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function useWalletData(initialValue) {
-  const [walletData, setWalletData] = useState(initialValue);
-
-  return [walletData, setWalletData];
-}
