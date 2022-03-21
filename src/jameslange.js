768db2730489b428085867b3f54da283b7300c3e
jameslange <jlange101592@gmail.com>
2022-03-21 13:57:30 +0100

    newcommit

--- src/jameslange.js
+++ src/jameslange.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
- import {MoralisProvider} from 'react-moralis';
+import NftsFromWallet from "./components/NFTComponents/NftsFromWallet";
+import "./index.js";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/crypto-app-1" element={<Home />} />
+          <Route path="/searchnftsbywallet" element={<NftsFromWallet />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
-
-ReactDOM.render(
- 
-  <MoralisProvider appId="fEzA76FoMU2hV7NRGHTC92wli93E7dz6G6gWPKyQ" serverUrl="https://scm8z1ukvds8.usemoralis.com:2053/server"> 
-    
-      <App />
-   
-  </MoralisProvider> 
-,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
